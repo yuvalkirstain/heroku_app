89299797d087cec39b5e39cd942bfa5c33117c90
yuvalkirstain <kirstain.yuval@gmail.com>
2022-12-15 13:36:57 +0200

    fix - upload image to aws as background job

--- sql_db/__init__.py
+++ sql_db/__init__.py
@@ -17,63 +17,25 @@ from sql_db.users import create_user_table, add_user, get_all_users, get_user_by
 from sql_db.rankings import create_rankings_table, add_ranking, get_all_rankings, RankingData
 from utils import logger
 
-# logger.debug(f"Downloading Data from repo")
-#
-# REPO_PATH = "data"
-# if not os.path.exists(REPO_PATH):
-#     Repo.clone_from(REPO_NAME, REPO_PATH)
-#     repo = Repo('data')
-# else:
-#     repo = Repo('data')
-#     repo.remotes.origin.pull()
-#
-# logger.debug("Finished git pull")
+
+def upload_images():
+    logger.debug(f"Starting updating images - {datetime.now()}")
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+    )
+    for path in glob(f"{IMAGE_DIR}/*"):
+        s3_client.upload_file(path,
+                              BUCKET_NAME,
+                              path,
+                              ExtraArgs=S3_EXTRA_ARGS)
+        os.remove(path)
+    logger.debug(f"Finished updating images - {datetime.now()}")
 
 
-# def download_db():
-#     if os.path.exists(HF_DB_PATH):
-#         shutil.copyfile(HF_DB_PATH, DB_FILE)
-#
-#
-# def table2csv(table_name):
-#     db = sqlite3.connect(DB_FILE)
-#     reviews = db.execute(f"SELECT * FROM {table_name}").fetchall()
-#     csv_path = f"{HF_DB_DIR}/{table_name}.csv"
-#     pd.DataFrame(reviews).to_csv(csv_path, index=False)
-#     repo.index.add(os.path.realpath(csv_path))
-#
-#
-# def backup_db():
-#     logger.info(f"Starting updating db - {datetime.now()}")
-#     shutil.copyfile(DB_FILE, HF_DB_PATH)
-#     repo.index.add(os.path.realpath(HF_DB_PATH))
-#     table2csv("users")
-#     table2csv("images")
-#     table2csv("rankings")
-#     repo.index.commit(f"Updating data at {datetime.now()}")
-#     repo.git.push()
-#     logger.info(f"Finished updating db - {datetime.now()}")
-
-#
-# def upload_images():
-#     logger.debug(f"Starting updating images - {datetime.now()}")
-#     s3_client = boto3.client(
-#         's3',
-#         aws_access_key_id=AWS_ACCESS_KEY,
-#         aws_secret_access_key=AWS_SECRET_KEY,
-#     )
-#     for path in glob(f"{IMAGE_DIR}/*"):
-#         s3_client.upload_file(path,
-#                               BUCKET_NAME,
-#                               path,
-#                               ExtraArgs=S3_EXTRA_ARGS)
-#         os.remove(path)
-#     logger.debug(f"Finished updating images - {datetime.now()}")
-
-
-# logger.debug("Setting up background jobs")
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=backup_db, trigger="interval", seconds=128)
-# scheduler.add_job(func=upload_images, trigger="interval", seconds=128)
-# scheduler.start()
-# logger.debug("Finished setting up background jobs")
+logger.debug("Setting up background jobs")
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=upload_images, trigger="interval", seconds=60)
+scheduler.start()
+logger.debug("Finished setting up background jobs")
