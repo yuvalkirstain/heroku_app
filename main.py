82142f69d7d358a11000e2f4aa7b41a53a092ed0
yuvalkirstain <kirstain.yuval@gmail.com>
2023-01-12 19:58:52 +0200

    add twitter button - trying to add more requirements and logging

--- main.py
+++ main.py
@@ -416,13 +416,18 @@ async def tweet_images(tweet: TweetRequest):
     prompt = tweet.prompt
     image_data = tweet.image_data
     user_id = tweet.user_id
+    logger.debug(f"TWEET - inside tweet images")
     image = Image.open(BytesIO(base64.b64decode(image_data)))
     image.save(f"images/{image_uid}.png")
     tweet_text = f"{prompt} https://pickapic.io/ Join the effort!"
+    logger.debug(f"tweeting {tweet_text=}")
+    logger.debug(f"TWEET - before tweeting")
     status = twitter_api.update_status_with_media(tweet_text, f"images/{image_uid}.png")
+    logger.debug(f"TWEET - after tweeting")
     os.remove(f"images/{image_uid}.png")
     tweet_text = f"{status.text} %23PickaPic %40PickaPicTweet"
     tweet_text = f"{tweet_text.replace(' ', '+')}"
+    logger.debug(f"TWEET - returning text")
     return {"status": "ok", "tweet_text": tweet_text}
 
 
