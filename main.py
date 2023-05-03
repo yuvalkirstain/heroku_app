7019345614acb331fda363d40d82b445ca068644
yuvalkirstain <kirstain.yuval@gmail.com>
2023-05-03 11:10:27 +0300

    change IPCInline

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ async def generate_images_via_api(prompt, negative_prompt, user_id, engine_id):
         num_tries = 0
         seed = random.randint(0, 2147483647)
         gs = random.uniform(3, 12)
-        asscore = 6.750
+        asscore = random.choice([6.0, 6.25, 6.5, 6.75, 7.0, 7.25, 7.5, 7.75])
         api_host = f"{STABILITY_API_HOST}/v1beta/generation/{engine_id}/text-to-image"
         if engine_id == STABILITY_ENGINE_ID_2:
             res = 512
